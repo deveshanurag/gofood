4e0961ceb19cdd9a3fc9512f48e7a2e360650716
Devesh Kumar <115335701+deveshanurag@users.noreply.github.com>
2024-08-22 16:43:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000
 const mongoDB = require('./db')
 mongoDB();
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","https://gofood-frontend-gvr6.onrender.com");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
