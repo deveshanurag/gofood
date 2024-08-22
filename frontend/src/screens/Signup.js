9386765749b479a74febdc480e69b24fc0f9f0a7
Devesh Kumar <115335701+deveshanurag@users.noreply.github.com>
2024-08-22 16:00:35 +0530

    Update Signup.js

--- frontend/src/screens/Signup.js
+++ frontend/src/screens/Signup.js
@@ -22,7 +22,7 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://gofood-backend-gx42.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
